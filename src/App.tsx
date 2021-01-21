2ac7cc7313636e0f3f97c2851cb5af6870ae0d56
quintonlucyk <quinton_lucyk@yahoo.com>
2021-01-21 12:01:37 -0500

    requests setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import EllenContent from "./tabs/EllenContent";
 import "./App.css";
 import { SampleAppleData } from "./sample-responses/AppleData";
 import { SampleATATData } from "./sample-responses/ATATData";
+import { SampleFMPIncomeGrowth } from "./sample-responses/FMPIncomeGrowth";
+import { SampleFMPCashGrowth } from "./sample-responses/FMPCashGrowth";
+import { SampleFMPBalanceGrowth } from "./sample-responses/FMPBalanceGrowth";
+import { SampleFMPKeyMetrics } from "./sample-responses/FMPKeyMetrics";
 
 const inDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
@@ -13,6 +17,10 @@ export default function App() {
   const [input, setInput] = React.useState("");
   const [symbol, setSymbol] = React.useState("");
   const [data, setData] = React.useState({});
+  const [fmpIncomeGrowth, setFMPIncomeGrowth] = React.useState([{}]);
+  const [fmpKeyMetrics, setFMPKeyMetrics] = React.useState([{}]);
+  const [fmpCashGrowth, setFMPCashGrowth] = React.useState([{}]);
+  const [fmpBalanceGrowth, setFMPBalanceGrowth] = React.useState([{}]);
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -53,8 +61,64 @@ export default function App() {
         setData({});
       }
     };
-    if (!!symbol && window.location.hash.includes("ellen")) {
-      fetchEllenHistoricalData(symbol);
+
+    const fetchQuintonHistoricalData = async (symbol: string) => {
+      const FMPAPI_KEY = process.env.REACT_APP_FMPAPI_KEY || "";
+      setIsLoading(true);
+      if (inDev) {
+        setIsLoading(false);
+        setFMPIncomeGrowth(SampleFMPIncomeGrowth);
+        setFMPBalanceGrowth(SampleFMPBalanceGrowth);
+        setFMPCashGrowth(SampleFMPCashGrowth);
+        setFMPKeyMetrics(SampleFMPKeyMetrics);
+        setIsError(false);
+        return;
+      }
+
+      const fmpRequests = [
+        fetch(
+          `https://fmpcloud.io/api/v3/income-statement-growth/${symbol}?limit=10&apikey=${FMPAPI_KEY}`
+        ),
+        fetch(
+          `https://fmpcloud.io/api/v3/key-metrics/${symbol}?limit=11&apikey=${FMPAPI_KEY}`
+        ), //Need 11 roic data points to be able to calculate 10 columns of growth
+        fetch(
+          `https://fmpcloud.io/api/v3/balance-sheet-statement-growth/${symbol}?limit=10&apikey=${FMPAPI_KEY}`
+        ),
+        fetch(
+          `https://fmpcloud.io/api/v3/cash-flow-statement-growth/${symbol}?limit=10&apikey=${FMPAPI_KEY}`
+        ),
+      ];
+
+      Promise.all(fmpRequests)
+        // map array of responses into an array of response.json() to read their content
+        .then((responses) => Promise.all(responses.map((r) => r.json())))
+        // all JSON answers are parsed: "jsonResponses" is the array of them
+        .then((jsonResponses) => {
+          setIsLoading(false);
+          setIsError(false);
+          setFMPIncomeGrowth(jsonResponses[0]);
+          setFMPKeyMetrics(jsonResponses[1]);
+          setFMPBalanceGrowth(jsonResponses[2]);
+          setFMPCashGrowth(jsonResponses[3]);
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setIsError(true);
+          setFMPIncomeGrowth([{}]);
+          setFMPKeyMetrics([{}]);
+          setFMPBalanceGrowth([{}]);
+          setFMPCashGrowth([{}]);
+          console.error(error.message);
+        });
+    };
+
+    if (!!symbol) {
+      if (window.location.hash.includes("ellen")) {
+        fetchEllenHistoricalData(symbol);
+      } else {
+        fetchQuintonHistoricalData(symbol);
+      }
     }
   }, [symbol]);
 
@@ -65,7 +129,7 @@ export default function App() {
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
     if (input !== symbol) {
-      setSymbol(input);
+      setSymbol(input.toUpperCase());
     }
   };
 
@@ -105,7 +169,19 @@ export default function App() {
                   />
                 )}
               />
-              <Route path="/" render={() => <QuintonContent />} />
+              <Route
+                path="/"
+                render={() => (
+                  <QuintonContent
+                    fmpIncomeGrowth={fmpIncomeGrowth}
+                    fmpBalanceGrowth={fmpBalanceGrowth}
+                    fmpCashGrowth={fmpCashGrowth}
+                    fmpKeyMetrics={fmpKeyMetrics}
+                    isLoading={isLoading}
+                    isError={isError}
+                  />
+                )}
+              />
             </Switch>
           </>
         )}
--- src/tabs/QuintonContent.tsx
+++ src/tabs/QuintonContent.tsx
@@ -1,7 +1,43 @@
 import * as React from "react";
 
-const QuintonContent: React.FC = () => {
-  return <div></div>;
-};
+// interface TableDataInterface {
+//   date?: string;
+//   roic?: number; //Key Metrics
+//   growthRevenue?: number; //Income Statement
+//   growthEPS?: number; //Income Statement
+//   growthTotalStockholdersEquity?: number; //Balance Sheet
+//   growthFreeCashFlow?: number; //CashFlowGrowth
+// }
+
+// interface FmpIncomeGrowthInterface {}
+
+interface Props {
+  fmpIncomeGrowth: any[];
+  fmpCashGrowth: any[];
+  fmpBalanceGrowth: any[];
+  fmpKeyMetrics: any[];
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const QuintonContent: React.FC<Props> = React.memo(
+  ({
+    fmpIncomeGrowth,
+    fmpCashGrowth,
+    fmpBalanceGrowth,
+    fmpKeyMetrics,
+    isLoading,
+    isError,
+  }) => {
+    /**
+     * ROIC
+     * Revenue
+     * EPS
+     * Equity
+     * FreeCashFlow
+     */
+    return <div></div>;
+  }
+);
 
 export default QuintonContent;
